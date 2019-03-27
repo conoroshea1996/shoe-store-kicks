a7e472aea7cf9781350082e36a6dedf561d22199
ConorOShea <conoshea12@gmail.com>
2019-03-27 08:49:54 +0000

    added cart functionality

--- js/app.js
+++ js/app.js
@@ -20,11 +20,72 @@
 
            if (event.target.parentElement.classList.contains('store-item-icon')){
 
-                console.log(event.target.parentElement.parentElement);
-                
+                let FilePath = event.target.parentElement.previousElementSibling.src;
+
+                let item = {};
+
+
+                item.img = FilePath;
+
+                let name = event.target.parentElement.parentElement.nextElementSibling.children[0].children[0].textContent;
+                item.name = name;
+
+                let Price = event.target.parentElement.parentElement.nextElementSibling.children[0].children[1].textContent;
+
+                let FinalPrice = Price.slice(1).trim();
+
+                item.price = FinalPrice;
+
+              const cartItem = document.createElement('div');
+
+              cartItem.classList.add('cart-item','d-flex','justify-content-between','text-capitalize','my-3');
+
+              cartItem.innerHTML = 
+                                    `<img src="${item.img} " id="item-img" alt="">
+                                    <div class="cart-item-text">
+                        
+                                        <p id="cart-item-title" class="font-weight-bold mb-0">${item.name}</p>
+                                        <span>$</span>
+                                        <span id="cart-item-price" class="cart-item-price" class="mb-0">${item.price}</span>
+                                    </div>
+                                    <a href="#" id='cart-item-remove' class="cart-item-remove">
+                                        <i class="fas fa-trash"></i>
+                                    </a>
+                                    </div>`;
+            
+                const cart = document.getElementById('cart');
+                const total = document.querySelector('.cart-total-container');
+
+                cart.insertBefore(cartItem,total);
+
+                alert('item added to cart');
+
+                showTotal();
+
 
            }
         });
     });
+
+        function showTotal(){
+            const total = [];
+            const items = document.querySelectorAll('.cart-item-price');
+            items.forEach(function(item){
+                total.push(parseFloat(item.textContent));
+                console.log(total);
+            });
+
+            const totalMoney = total.reduce(function(total,item){
+                    total += item;
+                    return total;
+
+            },0);
+            console.log(totalMoney);
+
+            document.getElementById('cart-total').textContent = totalMoney;
+            document.querySelector('.item-total').textContent = totalMoney;
+            document.getElementById('item-count').textContent = total.length;
+
+        }
 
 })();
